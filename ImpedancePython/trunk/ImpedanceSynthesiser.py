096ee3250864ba61faecfdcd5be655d624bb75d0
aalmeida <aalmeida@f74b19e9-7687-0410-8f26-8cda806574e3>
2016-02-03 06:32:36 +0000

    Resonator section calaulation functions

--- ImpedancePython/trunk/ImpedanceSynthesiser.py
+++ ImpedancePython/trunk/ImpedanceSynthesiser.py
@@ -1,5 +1,56 @@
+"""
+Functions to compute 1D input impedances of instruments 
+from geometrical data of a linear resontor.
+
+Uses global parameters contained in phys_params
+
+
+"""
+
 import numpy as np
 import matplotlib.pyplot as pl
+
+
+
+phys_params = {'speed_of_sound': 343.2,
+               'medium_density': 1.2}
+               
+constants = {'dry_air_molar_mass': 0.02897,   # kg/mol 
+             'water_molar_mass': 0.018016,    # kg/mol
+             'gas_constant': 8.314,           # J/K/mol
+             'dry_air_gas_constant': 287.058, 
+             'vapour_gas_constant': 461.495,
+             'celsius_to_kelvin': 273.15}
+
+def water_vapour_pressure(temperature=25., humidity=0.5):
+    saturation_pressure = 6.1078 * 10.**(7.5*temperature/(temperature+237.3))
+    return saturation_pressure * humidity
+
+def calc_params(temperature=25., humidity=0.5, ambient_pressure=101325.):
+    '''Recalculates physical parameters based on ambient values of:
+    * temperature (in C)
+    * humidity (in fraction i.e. 0.5 rather than 50%)
+    * ambient_pressure (in Pa)'''
+    
+    global phys_params
+    global constants
+    
+    kelvin = temperature + constants['celsius_to_kelvin']
+    
+    vapour_pressure = water_vapour_pressure(temperature, humidity)
+    
+    dry_pressure = ambient_pressure - vapour_pressure
+    dry_density = dry_pressure / kelvin /  constants['dry_air_gas_constant']
+    
+    vapour_density = vapour_pressure / kelvin /  constants['vapour_gas_constant']
+    
+    phys_params['medium_density'] = vapour_density + dry_density
+    
+    # simplistic, for now... (Wikipedia)
+    #molar_ratio =  (1-humidity) + humidity * constants['water_molar_mass'] / constants['dry_air_molar_mass']
+    #phys_param['speed_of_sound'] = (331.3 + 0.606 * temperature) / np.sqrt(molar_ratio)
+    phys_param['speed_of_sound'] = 331.3 + np.sqrt(1 + temperature/ constants['celsius_to_kelvin']) 
+                                         + 1.24 * humidity
 
 class DuctSection:
     '''Base class for a 1D resonator section'''
@@ -15,31 +66,44 @@ class DuctSection:
 class MiddleSection(self):
     '''Any section connecting to another section in either end'''
     def __init__(self, **kwargs):
-        self.__SetTravellingWaveTransferMatrix(**kwargs)
+        if kwargs:
+            self.__SetTravellingWaveTransferMatrix(**kwargs)
+        else:
+            self.__SetTravellingWaveTransferMatrix(tmOO=1.0, tmOI=0.0, tmIO=0.0, tmII=1.0)
         
-    def ApplyTransferMatrix(self, inMattrix):
+    def ApplyTransferMatrix(self, inMattrix, freq):
         '''Calculates the transfer matrix to apply 
         to the downstream sections
         
-        in Mattrix is the down stream transfer mattrix'''
+        inMattrix is the downstream transfer mattrix
+        freq is the frequency vector at which to apply the mattrix'''
         try:
             
+            
+    def TransferMattrixAtFreq(self, freq=0.0):
+        try:
+            functional_mx = [[self.tmOO, self.tmOI], 
+                             [self.tmIO, self.tmII]]
+            mx = [[el(freq) for el in row] for row in functional_mx]
+            return np.mattrix(mx)
+        except AttributeError:
+            raise
     
     def __SetTravellingWaveTransferMatrix(self, **kwargs):
         '''Set the transfer matrix in terms of travelling wave coefficients
         
-        MiddleSection.__SetTravellingWaveTransferMatrix(self, tmOO=1, tmOI=0, tmIO=0, tmII=1)
-            _     _ _            _ _     _
-            | pO1 | | tmOO  tmOI | | pO2 |
-        M = |     | |            | |     |
-            | pI1 | | tmIO  tmII | | pI2 |
-            -     - -            - -     -
+        MiddleSection.__SetTravellingWaveTransferMatrix(self, tmPP=1, tmUP=0, tmPU=0, tmUU=1)
+            _    _ _            _ _    _
+            | Pi | | tmPP  tmUP | | Po |
+        M = |    | |            | |    |
+            | Ui | | tmPU  tmUU | | Uo |
+            -    - -            - -    -
         
-        where PO1, PI1 are the outgoing incoming pressure waves at input
-        and PO2, PI2 at the output
+        where Pi, Ui are the pressure and flow at input
+        and Po, Uo at the output
         '''
         
-        mattrix_coeffs = ('tmOO','tmOI','tmIO','tmII',)
+        mattrix_coeffs = ('tmPP','tmUP','tmPU','tmUU',)
         
         for key, value in kwargs.items():
             if key in mattrix_coeffs:
@@ -66,6 +130,7 @@ class TerminationImpedance(DuctSection):
         fvec = np.linspace(fmin,fmax,npoints)
         pl.plot(fvec,self(fvec))
         return fig
+        
 
 class PerfectOpenEnd(TerminationImpedance):
     '''Ideal open end impedance
@@ -93,8 +158,35 @@ class StraightSection(MiddleSection):
         self.r_out  = r_out
         self.length = length
         
-        self.x1 = self.set_apex_distance()
-        self.x2 = self.set_base_distance()
+        self.apex_distance = self.set_apex_distance()
+        self.base_distance = self.set_base_distance()
+        self.cone_half_angle = np.atan(self.r_in / self.apex_distance)
+        
+    def CalcTransferMatrix(self):
+        global phys_params
+        
+        c = phys_params['speed_of_sound']
+        
+        apex_dist = self.apex_distance
+        cone_length = self.length + apex_dist
+        
+        radius_ratio = self.r_in / self.r_out
+        area_ratio = radius_ratio * radius_ratio
+        
+        apexial_wave_area = 2.*np.pi*self.apex_distance**2 * (1. - cos())
+        
+        # change here for wall losses
+        jkL = lambda ff: 2j*np.pi*ff/c * self.length
+        jkAD = lambda ff: 2j*np.pi*ff/c * apex_distance
+        jkCL2 = lambda ff: 2j*np.pi*ff/c * cone_length * cone_length
+        
+        tmPP = np.vectorize(lambda ff: area_ratio*(apex_dist/cone_length * np.cos(jkL(ff)) - 
+                                       apex_distance / (jkCL2(ff)) * np.sin(jkL(ff))))
+        tmUP =  np.vectorize(lambda ff: (apex_distance/cone_length) * 1j * np.sin(jkL(ff)))
+        tmPU = 
+        tmUU = np.vectorize(lambda ff: (apex_distance/cone_length) * (
+                                          np.cos(jkL(ff)) +
+                                          1./ (jkAD(ff)) * np.sin(jkL(ff))))
         
         
 class OneDResonator:
