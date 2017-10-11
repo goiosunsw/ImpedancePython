ade116766073fa57f972fccc14142a2eeee35837
aalmeida <aalmeida@f74b19e9-7687-0410-8f26-8cda806574e3>
2017-10-11 01:54:59 +0000

    impedance calulation changes

--- ImpedancePython/trunk/ImpedanceSynthesiser.py
+++ ImpedancePython/trunk/ImpedanceSynthesiser.py
@@ -23,7 +23,8 @@ constants = {'dry_air_molar_mass': 0.02897,   # kg/mol
              'celsius_to_kelvin': 273.15}
 
 def water_vapour_pressure(temperature=25., humidity=0.5):
-    saturation_pressure = 6.1078 * 10.**(7.5*temperature/(temperature+237.3))
+    saturation_pressure = 6.1078 * 10.**(7.5*temperature/(temperature+
+                                                          constants['celsius_to_kelvin']))
     return saturation_pressure * humidity
 
 def calc_params(temperature=25., humidity=0.5, ambient_pressure=101325.):
@@ -49,178 +50,340 @@ def calc_params(temperature=25., humidity=0.5, ambient_pressure=101325.):
     # simplistic, for now... (Wikipedia)
     #molar_ratio =  (1-humidity) + humidity * constants['water_molar_mass'] / constants['dry_air_molar_mass']
     #phys_param['speed_of_sound'] = (331.3 + 0.606 * temperature) / np.sqrt(molar_ratio)
-    phys_param['speed_of_sound'] = 331.3 + np.sqrt(1 + temperature/ constants['celsius_to_kelvin']) 
+    phys_param['speed_of_sound'] = 331.3 + np.sqrt(1 + temperature/ constants['celsius_to_kelvin']) \
                                          + 1.24 * humidity
 
-class DuctSection:
-    '''Base class for a 1D resonator section'''
-    def __init__(self):
-        pass
-    
-    
-    def __str__(self):
-        '''Prints the geometrical dimensions of this section'''
-        pass
+class AcousticWorld(object):
+    '''
+    Set of acoustic constant to be used in a synthesised impedance
+    '''
+    def __init__(self,temp=25., humid= 0.5, press = 101325.):
+        '''
+        Set a new acoustic world object:
+        
+        temp: temperature in deg C
+        humid: relative humidity 
+        press: ambient pressure in Pa
+        '''
+        self.temperature = temp
+        self.humidity = humid
+        self.pressure = press
+        
+        
+        #self.speed_of_sound = 343.2
+        #self.medium_density =  1.2
+        
+        self._recalc()
+        
+    def _recalc(self):
+        self.speed_of_sound = 331.3 * np.sqrt(1 + self.temperature/ 
+                                              constants['celsius_to_kelvin']) \
+                                    + 1.24 * self.humidity
+        kelvin = self.temperature + constants['celsius_to_kelvin']
+        
+        vapour_pressure = water_vapour_pressure(self.temperature, 
+                                                self.humidity)
+        
+        dry_pressure = self.pressure - vapour_pressure
+        dry_density = dry_pressure / \
+                      kelvin / \
+                      constants['dry_air_gas_constant']
+        
+        vapour_density = vapour_pressure / \
+                         kelvin /  \
+                         constants['vapour_gas_constant']
+        
+        self.medium_density = vapour_density + dry_density
+            
+        # boundary layer constants give ratios are used to 
+        # calculate rv and rt constants for effective speed calculations
+        # rx = const*radius*f^1/2*
+        # from Scavone : needs more work
+        #self.viscous_boundary_layer_const = 632.8*(1-0.0029*self.temperature-300)
+        #self.thermal_boundary_layer_const = 532.2*(1-0.0031*self.temperature-300)
+        
+        self.viscous_boundary_layer_const = np.sqrt(2*np.pi/self.speed_of_sound/4e-8)
+        self.thermal_boundary_layer_const = np.sqrt(2*np.pi/self.speed_of_sound/5.6e-8)
         
        
-class MiddleSection(self):
+class DuctSection(object):
     '''Any section connecting to another section in either end'''
-    def __init__(self, **kwargs):
-        if kwargs:
-            self.__SetTravellingWaveTransferMatrix(**kwargs)
-        else:
-            self.__SetTravellingWaveTransferMatrix(tmOO=1.0, tmOI=0.0, tmIO=0.0, tmII=1.0)
+    def __init__(self):
+        '''Initialise a generic middle section portion
         
-    def ApplyTransferMatrix(self, inMattrix, freq):
-        '''Calculates the transfer matrix to apply 
-        to the downstream sections
-        
-        inMattrix is the downstream transfer mattrix
-        freq is the frequency vector at which to apply the mattrix'''
-        try:
-            
-            
-    def TransferMattrixAtFreq(self, freq=0.0):
-        try:
-            functional_mx = [[self.tmOO, self.tmOI], 
-                             [self.tmIO, self.tmII]]
-            mx = [[el(freq) for el in row] for row in functional_mx]
-            return np.mattrix(mx)
-        except AttributeError:
-            raise
-    
-    def __SetTravellingWaveTransferMatrix(self, **kwargs):
-        '''Set the transfer matrix in terms of travelling wave coefficients
-        
-        MiddleSection.__SetTravellingWaveTransferMatrix(self, tmPP=1, tmUP=0, tmPU=0, tmUU=1)
-            _    _ _            _ _    _
-            | Pi | | tmPP  tmUP | | Po |
-        M = |    | |            | |    |
-            | Ui | | tmPU  tmUU | | Uo |
-            -    - -            - -    -
-        
-        where Pi, Ui are the pressure and flow at input
-        and Po, Uo at the output
+        Default is a section that does not change presure and flow
+        (0-length cylinder)
         '''
+        pass
         
-        mattrix_coeffs = ('tmPP','tmUP','tmPU','tmUU',)
+    def _chain_reflection_coeff_at_freq(self, r_in, freq):
+        '''Calculate reflection coefficient at beginning of section,
+        when section is chained to a termination 
+        with reflection coeff r_in'''
         
-        for key, value in kwargs.items():
-            if key in mattrix_coeffs:
-            try:
-                value(0.0)
-                setattr(self, key, value)
-            except TypeError:
-                setattr(self, key, np.vectorize(lambda x: value))
+        tmx = self.travelling_mx_at_freq(freq)
+        
+        p_out = tmx[0,0]*1 + tmx[0,1]*r_in
+        p_in  = tmx[1,0]*1 + tmx[1,1]*r_in  
+        
+        return p_in/p_out
+            
+    def _chain_impedance_at_freq(self, z_end, freq):
+        '''Calculate reflection coefficient at beginning of section,
+        when section is chained to a termination 
+        with reflection coeff r_in'''
+        
+        tmx = self.transfer_mx_at_freq(freq)
+        
+        p_st = tmx[0,0]*z_end + tmx[0,1]*1
+        u_st  = tmx[1,0]*z_end + tmx[1,1]*1  
+        
+        if p_st == np.inf:
+            if u_st!=0.0:
+                return np.inf
+            else:
+                raise ZeroDivisionError
+        
+        if u_st!=0.0:
+            return p_st/u_st
+        else:
+            return np.inf
+        
+            
+    def travelling_mx_at_freq(self, freq=0.0):
+        ''' return the transfer matrix of the section 
+        at a given frequency value:
+           relates [P+,P-] at each end'''
+
+
+        return np.array([[1,0],[0,1]])
+    
+    def transfer_mx_at_freq(self, freq=0.0):
+        ''' return the transfer matrix of the section 
+        at a given frequency value:
+           relates [P,U] at each end'''
+
+        return np.array([[1,0],[0,1]])
+    
+    def _recalc(self):
+        pass
+    
+    def get_speed_of_sound(self):
+        if self.parent:
+            return self.parent.speed_of_sound
+        else:
+            return AcousticWorld().speed_of_sound
+
+    def get_medium_density(self):
+        if self.parent:
+            return self.parent.medium_density
+        else:
+            return AcousticWorld().medium_density
+            
+    def get_boundary_layer_constants(self):
+        if self.parent:
+            world = self.parent.world
+            
+        else:
+            world = AcousticWorld()
+        return world.viscous_boundary_layer_const,\
+               world.thermal_boundary_layer_const
+    
+    def set_parent(self, parent):
+        self.parent=parent
+        self._recalc()
+
+
+class StraightDuct(DuctSection):
+    def __init__(self, length = 0.5, radius = 0.1):
+        self.radius = radius
+        self.length = length
+        self.parent = None
+        
+        self._recalc()
+        self.gamma = 1.4
+        
+    def _recalc(self):
+        self.cross_section = np.pi*self.get_input_radius()**2
+        self.char_impedance = self.get_characteristic_impedance()
+        rvc_per_rad, rtc_per_rad = self.get_boundary_layer_constants() 
+        self.rv_const = rvc_per_rad * self.radius
+        self.rt_const = rtc_per_rad * self.radius
+        if self.parent:
+            self.losses=self.parent.losses
+        else:
+            self.losses=True
+    
+    def get_propagation_coefficient(self, freq):
+        if not self.losses:
+            return 2*np.pi*freq/self.get_speed_of_sound()
+        else:
+            return self._propagation_coeff(freq)
+
+    
+    def _propagation_coeff(self, freq):
+        c = self.get_speed_of_sound()
+        rho = self.get_medium_density()
+        
+        omega   = 2  * np.pi * freq
+        k       = omega/c
+        
+        rv      = self.rv_const * np.sqrt(freq)
+        rt      = self.rt_const * np.sqrt(freq)
+
+        P       = np.sqrt(2)/rv
+        PQ      = (self.gamma-1)*np.sqrt(2)/rt;
+        Zv      = omega*rho*(P*(1.+3.*P/2.)+1j*(1.+P));
+        Yt      = omega/(rho*c**2)*\
+                        (PQ*(1.-PQ/(2*(self.gamma-1.)))+1j*(1.+PQ));
+        
+        # propagation constant
+        G       = np.sqrt(Zv*Yt) 
+        # characteristic impedance
+        # Zeta    = np.sqrt(Zv/Yt)/s  
+        
+        return -1j*G
+
+    
+    def get_input_radius(self):
+        return self.radius
+        
+    def get_output_radius(self):
+        return self.radius
+        
+    def get_length(self):
+        return self.length
+            
+    def travelling_mx_at_freq(self, freq=0.0):
+        
+        phase = 2*np.pi*freq*self.length/self.get_speed_of_sound()
+        
+        return np.array([[np.exp(1j*phase),0],[0,np.exp(-1j*phase)]])
+
+    def transfer_mx_at_freq(self, freq=0.0):
+        
+        
+        prop_coeff = self.get_propagation_coefficient(freq)
+        phase = prop_coeff*self.length
+        
+        return np.array([[np.cos(phase),
+                            1j*self.char_impedance*np.sin(phase)],
+                         [1j/self.char_impedance*np.sin(phase),
+                            np.cos(phase)]])
+                            
+    def get_characteristic_impedance(self):
+        return self.get_medium_density()*\
+                          self.get_speed_of_sound()/\
+                          self.cross_section
+
         
 class TerminationImpedance(DuctSection):
-    '''Base class for a termination impedance'''
+    '''Base class for a termination impedance
+    default is an open termination'''
     def __init__(self):
-        self.zl = np.vectorize(lambda x: 0.0)
+        self.__call__ = np.vectorize(self._get_impedance_at_freq)
         
-    def __call__(self, freq):
-        '''Return the complex value of the impedance at 
-        frequency'''
+    def __call__(self, f):
+        return f
         
-        return self.zl(freq)
+    def _get_reflection_coeff_at_freq(self, freq):
+        return -1.
         
-    def plot(self, fig=None, fmin=0.0, fmax=4000.0, npoints=200):
+    def _get_impedance_at_freq(self, f):
+        r = self._get_reflection_coeff_at_freq(f)
+        if r!=1.0:
+            return (1.+r)/(1.-r)
+        else:
+            return np.inf
+        
+    def plot_impedance(self, fig=None, fmin=0.0, fmax=4000.0, npoints=200):
         if not fig:
-            fig=pl.figure
+            fig,ax=pl.subplots(2, sharex=True)
         fvec = np.linspace(fmin,fmax,npoints)
-        pl.plot(fvec,self(fvec))
-        return fig
+        zvec = np.array([self._get_impedance_at_freq(f) for f in fvec])
+        
+        ax[0].plot(fvec,np.abs(zvec))
+        ax[1].plot(fvec,np.angle(zvec))
+        return fig,ax
         
 
 class PerfectOpenEnd(TerminationImpedance):
-    '''Ideal open end impedance
-    Load impedance Zl(f) = 0
-    Reflection function R(f) = -1 '''
-    def __init__(self):
-        self.zl = np.vectorize(lambda x: 0.0)
-
+    pass
 
 class PerfectClosedEnd(TerminationImpedance):
     '''Ideal open end impedance
     Load impedance Zl(f) = 0
     Reflection function R(f) = 1 '''
-    def __init__(self):
-        self.zl = np.vectorize(lambda x: np.Inf)
+    def _get_reflection_coeff_at_freq(self,freq):
+        return 1.
 
-
-class StraightSection(MiddleSection):
-    '''A straight tube element, either conical or cylindrical'''
-    def __init__(self, r_in, r_out, length):
-        '''Initialise duct section, with parameters:
-        * r_in, r_out: input and output radiuses
-        * length: (of the duct section)'''
-        self.r_in   = r_in
-        self.r_out  = r_out
-        self.length = length
-        
-        self.apex_distance = self.set_apex_distance()
-        self.base_distance = self.set_base_distance()
-        self.cone_half_angle = np.atan(self.r_in / self.apex_distance)
-        
-    def CalcTransferMatrix(self):
-        global phys_params
-        
-        c = phys_params['speed_of_sound']
-        
-        apex_dist = self.apex_distance
-        cone_length = self.length + apex_dist
-        
-        radius_ratio = self.r_in / self.r_out
-        area_ratio = radius_ratio * radius_ratio
-        
-        apexial_wave_area = 2.*np.pi*self.apex_distance**2 * (1. - cos())
-        
-        # change here for wall losses
-        jkL = lambda ff: 2j*np.pi*ff/c * self.length
-        jkAD = lambda ff: 2j*np.pi*ff/c * apex_distance
-        jkCL2 = lambda ff: 2j*np.pi*ff/c * cone_length * cone_length
-        
-        tmPP = np.vectorize(lambda ff: area_ratio*(apex_dist/cone_length * np.cos(jkL(ff)) - 
-                                       apex_distance / (jkCL2(ff)) * np.sin(jkL(ff))))
-        tmUP =  np.vectorize(lambda ff: (apex_distance/cone_length) * 1j * np.sin(jkL(ff)))
-        tmPU = 
-        tmUU = np.vectorize(lambda ff: (apex_distance/cone_length) * (
-                                          np.cos(jkL(ff)) +
-                                          1./ (jkAD(ff)) * np.sin(jkL(ff))))
-        
-        
-class OneDResonator:
-    '''A one dimensional resonator,
-    incorporating several elements'''
+class PortImpedance(object):
+    '''
+    Main functionality for an object with an input port
+    '''
+    
     def __init__(self):
-        self.middle_elements = []
-        self.termination = None
-        self.set_perfect_open_pipe()
-        
-    def set_perfect_open_pipe(self):
-        '''Set the termination impedance to a static pressure
-        (Zl = 0)'''
-        self.termination = PerfectOpenEnd()
-        
-    def set_perfect_closed_pipe(self):
-        '''Set the termination impedance to a rigid wall
-        (Zl = Inf)'''
-        self.termination = PerfectClosedEnd()
-        
-    def get_input_impedance(self):
         pass
+    
+    def get_input_impedance_at_freq(self,f):
+        '''
+        Retrieve the input impedance at a particular value of frequency
+        '''
+        return 0.0
+
+class Duct(PortImpedance):
+    '''
+    1-D duct object containing linear elements
+    '''
+    def __init__(self, world=None, losses=True):
+        if not world:
+            world = AcousticWorld()
+
+        self.set_acoustic_world(world)
+        self.elements = []
+        self.termination = PerfectOpenEnd()
+        self.world = world
+        self.losses = losses
         
-    def draw(self):
-        '''Draw resonator in a window'''
-        fig = pl.figure()
-        fig.canvas.set_window_title('Test')
-        return fig
+    def set_acoustic_world(self,world):
+        self.speed_of_sound = world.speed_of_sound
+        self.medium_density = world.medium_density
         
-    def plot_impedance(self, fig=None, fmin=0.0, fmax=4000.0, npoints=200):
-        '''Plot the input impedance of the complete resonator'''
-        if not fig:
-            fig=pl.figure
-        fvec = np.linspace(fmin,fmax,npoints)
-        pl.plot(fvec,self.get_input_impedance(fvec))
-        return fig
+    def append_element(self, element):
+        assert isinstance(element, DuctSection)
+        element.set_parent(self)
+        self.elements.append(element)
         
+    def insert_element(self, element, pos):
+        assert isinstance(element, DuctSection)
+        element.set_parent(self)
+        self.elements.insert(element, pos)
+        
+    def set_termination(self, term):
+        assert isinstance(term, TerminationImpedance)
+        self.termination = term
+        
+    def get_input_reflection_function_at_freq(self, f):
+        r = self.termination._get_reflection_coeff_at_freq(f)
+        for el in reversed(self.elements):
+            r = el._chain_reflection_coeff_at_freq(r, f)
+        return r
+    
+    def get_input_impedance_at_freq(self, f):
+        z = self.termination._get_impedance_at_freq(f)
+        for el in reversed(self.elements):
+            z = el._chain_impedance_at_freq(z, f)
+        return z
+        
+    def get_coords(self):
+        old_x=0
+        x=[]
+        y=[]
+        for el in self.elements:
+            x.append(old_x)
+            y.append(el.get_input_radius())
+            x.append(x[-1]+el.get_length())
+            y.append(el.get_output_radius())
+            old_x = x[-1]
+
+        return x,y
