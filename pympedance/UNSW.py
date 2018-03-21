3c7450832a7b48f85a32286c716bb439823919c7
Andre Almeida <a.almeida@unsw.edu.au>
2018-03-21 23:33:15 +1100

    unsw object start

--- pympedance/UNSW.py
+++ pympedance/UNSW.py
@@ -13,6 +13,7 @@
 import string
 import sys
 import os
+import warnings
 import numpy as np
 from scipy.io import matlab
 import scipy.signal as sig
@@ -37,6 +38,272 @@ def read_UNSW_impedance(filename=None, paramfile=None,
     imp_obj.set_name(imp_meas.name)
 
     return imp_obj
+
+class MeasurementParameters(object):
+    def __init__(radius=0.0075,
+                 mic_spacing=None,
+                 attenuation_factor=1,
+                 driver='',
+                 wool=True,
+                 sampling_frequency=44100.,
+                 bit_depth=16,
+                 num_points=1024,
+                 desired_freq_lo=0.,
+                 desired_freq_hi=None,
+                 n_loops=1,
+                 n_channel_first=1,
+                 n_channel_last=3,
+                 temperature=25,
+                 humidity=0.6,
+                 gain=0.00316,
+                 computer='win',
+                 device_type='asio',
+                 device_number=0.,
+                 calibration_files=None):
+        pass
+                 
+
+
+class ImpedanceIteration(object):
+    """
+    Implements a single iteration of a measurement,
+    with a particular output waveform and collected data
+    """
+    def __init__(self, input_signals, output_signal=None, 
+                 n_loops=None, parameters=None):
+        """
+        start iteration object with the input (measured) signals,
+        which is the response of the system to an output_signal.
+
+        The output signal is looped n_loops times 
+
+        input_signal is N_samples X N_channels
+        """
+
+        self.input_signals = input_signals
+        self.has_output = False
+        self._output_signal = None
+        self.n_samples = self.input_signals.shape(0)
+        self.n_channels = self.input_signals.shape(1)
+        if n_loops is None:
+            n_loops = np.floor(self.n_samples / 
+                                      len(self.output_signal))
+        self.loop_n_samples = loop_n_samples
+        self.param = parameters
+
+    @property
+    def output_signal(self):
+        return self._output_signal
+
+    @output_signal.setter
+    def output_signal(self, signal):
+        if signal is not None:
+            self.has_output=True
+            self._output_signal = signal
+
+        if self.n_samples < len(self.ouput_signal):
+            raise ValueError("input_signal.shape[0] must be smaller than length of output")
+        
+        new_loop_n = np.floor(self.n_samples/len(self.output_signal)) 
+        if self.n_loops != new_loop_n:
+            warnings.warn("new number of loops adjusted to %d\n"
+                          % new_loop_n)
+            self.n_loops = new_loop_n
+
+    @property
+    def loop_length(self):
+        if self.output_signal:
+            return len(self.output_signal)
+        elif self.n_loops:
+            return int(self.n_samples/self.n_loops)
+        else:
+            raise ValueError("unkown number of loops")
+
+    @property
+    def mean_waveform(self):
+        sum_waveform = np.zeros((self.loop_length,self.input_signals[1]))
+        for ii in range(self.n_loops):
+            ist = ii*self.loop_length
+            iend = ist+self.loop_length
+            sum_waveform += self.input_signals[ist:iend]
+        return sum_waveform / self.n_loops
+
+    def get_mic_spectra_per_loop(self,
+                        nwind=1024,
+                        window=None,
+                        method='fft',
+                        nhop=None):
+        """
+        chops wavefile into excitation loops and calculates spectra
+
+        input_waves (N_samples x N_channels)
+
+        optional: 
+            discard_loops: remove N loops from beginning and end
+        
+        returns an array with (samples_per_loop/2+1) X N_loops X N_channels
+        """
+
+
+        nsamp = nwind
+        if window is not None:
+            wind = sig.get_window(window, nsamp)
+        else:
+            wind = np.ones(nsamp)
+
+        if nhop is None:
+            nhop = nsamp
+
+        all_spec = []
+        all_coh = []
+
+        for chno in range(0,input_waves.shape[1]):
+            chspec = []
+            if method == 'fft':
+                for n in range(0,input_waves.shape[0]-nsamp+1,nhop):
+                    w = input_waves[n:n+nsamp,chno]
+                    #wspec = np.fft.fft(w*wind)
+                    wspec = waveform_to_spectrum(w*wind)
+                    chspec.append(wspec[:int(nsamp/2+1)])
+                all_spec.append(chspec)
+            elif method == 'tf':
+                wspec = tfe(y=input_waves[:,chno],
+                            x=excitation_signal,
+                            nfft=nsamp, nhop=nhop,
+                            window=wind)
+                wcoh = sig.coherence(y=input_waves[:,chno],
+                            x=excitation_signal,
+                            nfft=nsamp, nhop=nhop,
+                            window=wind)
+                all_spec.append([wspec])
+                all_coh.append(wcoh)
+
+        return np.array(all_spec).transpose((2,1,0))
+
+    def calc_mean_spectra(self, discard_loops=0,
+                        nwind=1024,
+                        window=None,
+                        method='fft',
+                        nhop=None):
+        """
+        Returns mean spectra and spectral error
+        """
+        total_spectrum = self.get_mic_spectra_per_loop(nwind=nwind,
+                                                      window=window,
+                                                      method=method,
+                                                      nhop=nhop)
+        if discard_loops>0:
+            total_spectrum = total_spectrum[:,discard_loops:-discard_loops,:]
+        mean_spectrum = np.nanmean(total_spectrum, axis=1)
+        spectral_error = np.nanstd(total_spectrum, axis=1, ddof=1)
+        num_cycles = total_spectrum.shape[1]
+        
+        self.mean_spectrum = mean_spectrum
+        self.spectral_error = spectral_error
+        return mean_spectrum, spectral_error
+
+    def analyse_input(self):
+        """
+        Calculate pressure and flow at reference plane
+        """
+
+        rcond = -1
+        
+        param = self.param
+        num_cycles = param.num_cycles
+        noiseCalculated = numCycles >= 8
+        #  calculate A and b matrices
+        A = param.A
+        harmLo = param.harm_lo
+        harmHi = param.harm_hi
+        n_channel_first = param.n_channel_first
+        mic_spacing = param.mic_spacing
+        n_mics = len(mic_spacing)
+        # shouldn't it be...
+        # nChannelLast = nChannelFirst + nMics
+        n_channel_last = n_mics
+        fvec = param.frequency_vector
+
+        meanSpectrum = Input['meanSpectrum'].squeeze()
+        totalSpectrum = Input['totalSpectrum'].squeeze()
+        mean_spectrum, spectral_error = self.get_mean_spectra()
+
+        spectral_error = self.spectral_error[harmLo:harmHi+1, nChannelFirst-1:nChannelLast]
+
+        if noiseCalculated:
+            # Fit a function of the form Af^n to the noise data,
+            # and replace the noise data with the (smooth) function.
+            fitData = np.array([np.ones(fVec.shape), np.log(fVec)]).T
+            coeff,_,_,_ = lstsq(fitData,np.log(spectralError))
+            spectralError = np.exp(np.dot(fitData, coeff))
+            sigmaVariable = np.transpose(np.array([spectralError]),
+                                         axes=[2,0,1])
+        else:
+            # otherwise, choose a noise function of f^(-0.5)
+            sigmaVariable = (fVec**(-0.5) *
+                             np.ones((1,spectralError.shape[1])))
+            sigmaVariable = np.transpose(np.array([sigmaVariable]), 
+                                         axes=[2,0,1])
+        # pdb.set_trace()
+        if measType == 'Averaged':
+            meanSpecRed = meanSpectrum[harmLo:harmHi+1,nChannelFirst-1:nChannelLast]
+            b = np.transpose(np.array([meanSpecRed]),
+                             axes=[2,0,1])
+        elif measType == 'Individual':
+            # reorder the totalSpectrum vector to match meanSpectrum
+            if nMics == 1:
+                tempSpectrum =\
+                        totalSpectrum[harmLo:harmHi+1, countLoops:countLoops+1, 0];
+            elif nMics == 2:
+                tempSpectrum =\
+                    [totalSpectrum[harmLo:harmHi+1, countLoops:countLoops+1, 0],\
+                     totalSpectrum[harmLo:harmHi+1, countLoops:countLoops+1, 1]]
+            else:
+                tempSpectrum =\
+                    [totalSpectrum[harmLo: harmHi+1, countLoops:countLoops+1, 0],\
+                    totalSpectrum[harmLo: harmHi+1, countLoops:countLoops+1, 1],\
+                    totalSpectrum[harmLo: harmHi+1, countLoops:countLoops+1, 2]]
+
+            b = np.transpose(tempSpectrum, axes=[0,2,1])
+
+        # initialise p, u, deltap and deltau
+        p = np.zeros((A.shape[2],1), dtype='complex')
+        deltap = np.zeros((A.shape[2],1), dtype='complex')
+        u = np.zeros_like(p)
+        deltau = np.zeros_like(deltap)
+        for freqCount in range(A.shape[2]): # length of frequency vector
+            # Calculate the covariant matrix by putting the elements of
+            # sigmaVariable along the diagonal
+            covar = np.diag(sigmaVariable[:,0,freqCount]**2)
+            # if only two mics, calculate x using backslash operator
+            # i.e. solve A*x = B for x
+            if nMics == 1:
+                x,_,_,_ = lstsq(A[:,:,freqCount], b[:,:,freqCount])
+                # matrix is not square so use pseudoinverse
+                Ainv = pinv(A[:,:,freqCount])
+            elif nMics == 2:
+                x,_,_,_ = lstsq(A[:,:,freqCount], b[:,:,freqCount])
+                Ainv = np.linalg.inv(A[:,:,freqCount])
+            else:
+                # otherwise, use a weighted least-squares to determine x
+                # weighted least squares solution to A*x = b with weighting covar
+                x,_,_,_ = lscov(A[:,:,freqCount], b[:,0,freqCount], covar,
+                                rcond=rcond)
+                Ainv = pinv(A[:,:,freqCount])
+            # Use the inverse (or pseudoinverse) to calculate dx
+            dx = np.sqrt(np.diag(np.dot(np.dot(Ainv, covar),
+                                 Ainv.T)))
+            # Calculate the impedance and its error
+            Z0 = Parameters['Z0'].squeeze()
+            p[freqCount] = x[0]
+            u[freqCount] = x[1] / Z0
+            deltap[freqCount] = dx[0]
+            deltau[freqCount] = dx[1] / Z0
+
+        return dict(p=p, u=u, deltap=deltap, deltau=deltau, z0=Z0)
+
+
+        
 
 
 class ImpedanceMeasurement(object):
@@ -460,7 +727,7 @@ def resample_calibration(Parameters,
 
     new measurements will be based on Parameters['numPoints']/resamp
     points
-    """ 
+    """
     ParametersNew = deepcopy(Parameters)
     numPoints = np.asscalar(Parameters['numPoints'])
     harmLo = np.asscalar(Parameters['harmLo'])
