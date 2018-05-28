024f99be9ff1279cad42c0809d524ef7992b441b
Andre Almeida <a.almeida@unsw.edu.au>
2018-05-28 18:08:08 +1000

    disable individual calibrations

--- examples/recalc_calibration.py
+++ examples/recalc_calibration.py
@@ -11,6 +11,10 @@ parser.add_argument('filename', nargs='?', help='measurement filename')
 parser.add_argument('-p', '--pipe', help='set infinite pipe calibration')
 parser.add_argument('-i', '--impedance', help='set infinite impedance calibration')
 parser.add_argument('-f', '--flange', help='set infinite flange calibration')
+parser.add_argument('-P', '--no-pipe', action='store_true', 
+                    help='disable infinite pipe calibration')
+parser.add_argument('-F', '--no-flange', action='store_true', 
+                    help='disable infinite flange calibration')
 args = parser.parse_args()
 
 #import pdb
@@ -31,40 +35,48 @@ else:
 
 print('Using measurement file: '+meas_file)
 
+dict_convert = {'inf_imp': 'infinite_imp_file',
+                'inf_pipe': 'infinite_pipe_file',
+                'inf_flange': 'infinite_flange_file'}
+
 # Read the impedance file with its parameters
 io = ImpedanceMeasurement(meas_file)
 
 #calibs = io.parameters.find_calib_files()
 calibs = io.parameters.calib_files
-print('Calibration dict:')
-print(calibs)
+
+calib_args = dict()
+for k,v in dict_convert.items():
+    try:
+        calib_args[v] = calibs[k]
+    except KeyError:
+        calib_args[v] = None
+
+
 
 # The calibration files corresponding to the above measurement
 if args.impedance:
-    calib_infimp_file = args.impedance
-else:
-    calib_infimp_file = calibs['inf_imp']
-    #calib_infimp_file = os.path.join(script_path, '../tests/data/InfImpCalib.mat')
+    calib_args['infinite_imp_file'] = args.impedance
 if args.pipe:
-    calib_infpipe_file = args.pipe
-else:
-    calib_infpipe_file = calibs['inf_pipe']
-    #calib_infpipe_file = os.path.join(script_path, '../tests/data/InfPipeCalib.mat')
+    calib_args['infinite_pipe_file'] = args.pipe
 if args.flange:
-    calib_infflange_file = args.flange
-else:
-    calib_infflange_file = calibs['inf_flange']
-    #calib_infpipe_file = os.path.join(script_path, '../tests/data/InfPipeCalib.mat')
-calib_infimp_file = None
+    calib_args['infinite_flange_file'] = args.flange
+
+if args.no_pipe:
+    calib_args.pop('infinite_pipe_file',None) 
+if args.no_flange:
+    calib_args.pop('infinite_flange_file',None) 
+
+print('Calibration dict:')
+for k,v in calib_args.items():
+    print('  '+k+': '+v)
 
 # Plot it
 fig,ax = bodeplot(io.f,io.z)
 
 # Recalculate the calibration matrix
 old_a = io.parameters.A
-new_a = io.parameters.calc_calibration_matrix(infinite_imp_file=calib_infimp_file,
-                                             infinite_pipe_file=calib_infpipe_file,
-                                             infinite_flange_file=calib_infflange_file)
+new_a = io.parameters.calc_calibration_matrix(**calib_args)
 
 # Set the matrix in parameters
 # (will probably be done automatically in a later version)
@@ -74,6 +86,15 @@ new_z = io.calculate_impedance()
 
 # Plot the new impedance on top of the first one
 bodeplot(io.f,new_z, ax=ax)
+
+# Set the matrix in parameters
+# (will probably be done automatically in a later version)
+#io.parameters.A = old_a
+# recalculate the impedance with the new calibration
+#new_z = io.calculate_impedance()
+
+# Plot the new impedance on top of the first one
+#bodeplot(io.f,new_z, ax=ax)
 pl.show()
 
 
