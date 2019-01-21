6344a0648d08c60ff0650d7281880a236cc5b479
Andre Goios <goios@mus10im.local>
2019-01-21 18:27:08 +1100

    impedance converter and output to new file

--- examples/recalc_calibration.py
+++ examples/recalc_calibration.py
@@ -1,5 +1,11 @@
 import numpy as np
-import matplotlib.pyplot as pl
+try:
+    import matplotlib.pyplot as pl
+except ImportError:
+    import matplotlib
+    matplotlib.use('TkAgg')
+    import matplotlib.pyplot as pl
+    
 import sys
 import os
 import scipy.io as sio
@@ -8,6 +14,9 @@ import argparse as ap
 parser = ap.ArgumentParser()
 
 parser.add_argument('filename', nargs='?', help='measurement filename')
+parser.add_argument('-o', '--output', help='output to file')
+parser.add_argument('-r', '--radius', help='new impedance head radius', type=float)
+parser.add_argument('-c', '--calib', help='set calibration file')
 parser.add_argument('-p', '--pipe', help='set infinite pipe calibration')
 parser.add_argument('-i', '--impedance', help='set infinite impedance calibration')
 parser.add_argument('-f', '--flange', help='set infinite flange calibration')
@@ -15,6 +24,8 @@ parser.add_argument('-P', '--no-pipe', action='store_true',
                     help='disable infinite pipe calibration')
 parser.add_argument('-F', '--no-flange', action='store_true', 
                     help='disable infinite flange calibration')
+parser.add_argument('-N', '--no-recalibration', action='store_true', 
+                    help='disable recalibration')
 args = parser.parse_args()
 
 #import pdb
@@ -22,7 +33,8 @@ args = parser.parse_args()
 
 from pympedance.UNSW import *
 from pympedance.plot_utils import bodeplot
-
+    
+    
 # Define path for the data
 script_path, _ = os.path.split(os.path.realpath(__file__))
 print(script_path)
@@ -52,40 +64,64 @@ for k,v in dict_convert.items():
     except KeyError:
         calib_args[v] = None
 
+if args.output:
+    pass
+else:
+    # Plot it
+    fig,ax = bodeplot(io.f,io.z,label='original')
 
 
-# The calibration files corresponding to the above measurement
-if args.impedance:
-    calib_args['infinite_imp_file'] = args.impedance
-if args.pipe:
-    calib_args['infinite_pipe_file'] = args.pipe
-if args.flange:
-    calib_args['infinite_flange_file'] = args.flange
+if not args.no_recalibration:
+    if args.calib:
+        new_par = MeasurementParameters(args.calib)
+        old_a = io.parameters.A
+        new_a = new_par.A
+        print('Using calibration file: '+args.calib)
 
-if args.no_pipe:
-    calib_args.pop('infinite_pipe_file',None) 
-if args.no_flange:
-    calib_args.pop('infinite_flange_file',None) 
+    else:
+        # The calibration files corresponding to the above measurement
+        if args.impedance:
+            calib_args['infinite_imp_file'] = args.impedance
+        if args.pipe:
+            calib_args['infinite_pipe_file'] = args.pipe
+        if args.flange:
+            calib_args['infinite_flange_file'] = args.flange
 
-print('Calibration dict:')
-for k,v in calib_args.items():
-    print('  '+k+': '+str(v))
+        if args.no_pipe:
+            calib_args.pop('infinite_pipe_file',None) 
+        if args.no_flange:
+            calib_args.pop('infinite_flange_file',None) 
 
-# Plot it
-fig,ax = bodeplot(io.f,io.z)
+        print('Calibration dict:')
+        for k,v in calib_args.items():
+            print('  '+k+': '+str(v))
 
-# Recalculate the calibration matrix
-old_a = io.parameters.A
-new_a = io.parameters.calc_calibration_matrix(**calib_args)
 
+        # Recalculate the calibration matrix
+        old_a = io.parameters.A
+        new_a = io.parameters.calc_calibration_matrix(**calib_args)
+else:
+    new_a = io.parameters.A
+
+if args.radius:
+    io.parameters.radius = args.radius
+    
 # Set the matrix in parameters
 # (will probably be done automatically in a later version)
 io.parameters.A = new_a
 # recalculate the impedance with the new calibration
 new_z = io.calculate_impedance()
 
-# Plot the new impedance on top of the first one
-bodeplot(io.f,new_z, ax=ax)
+
+if args.output:
+    # z_new = io.iteration[-1].mean_impedance
+    io.save_mat(args.output)
+else:
+
+    # Plot the new impedance on top of the first one
+    bodeplot(io.f,new_z, ax=ax,label='corrected')
+    ax[1].legend()
+    pl.show()
 
 # Set the matrix in parameters
 # (will probably be done automatically in a later version)
@@ -95,7 +131,6 @@ bodeplot(io.f,new_z, ax=ax)
 
 # Plot the new impedance on top of the first one
 #bodeplot(io.f,new_z, ax=ax)
-pl.show()
 
 
 
